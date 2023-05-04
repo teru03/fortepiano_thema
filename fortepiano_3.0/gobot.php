635298e16fb4e7c96dadc2b5a5df7a701ffba3c3
teru03 <terusun@gmail.com>
2023-05-04 14:34:10 +0900

    delete not use files

--- fortepiano_3.0/gobot.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once dirname( __FILE__ ) . '/line_bot.php';
-
-line_message_scheduler_do();
-?>
--- fortepiano_3.0/gobot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /virtual/fortepiano/public_html/wordpress/wp-content/themes/fortepiano_3.0/
-/usr/bin/php -f gobot.php
